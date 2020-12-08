6331e390310af9453151460585197019fdfeb733
nathan-1206 <57306116+nathan-1206@users.noreply.github.com>
2020-12-08 17:55:20 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+
+window.onload = function () {
+    $('#header').animate({opacity: 1}, 1000);
+    $('#about-me').delay(500).animate({opacity: 1}, 1000);
+    $('#education').delay(1000).animate({opacity: 1}, 1000);
+    $('#experience').delay(1500).animate({opacity: 1}, 1000);
+    $('#contact').delay(2000).animate({opacity: 1}, 1000);
+    
+    $('#about-me-content').delay(500).animate({opacity: 1}, 1000);
+    
+    $('#georgia-tech-header').delay(500).animate({opacity: 1}, 1000);
+    $('#georgia-tech-content').delay(500).animate({opacity: 1}, 1000);
+    $('#minnesota-header').delay(1000).animate({opacity: 1}, 1000);
+    $('#minnesota-content').delay(1000).animate({opacity: 1}, 1000);
+
+    $('#paid-experience-header').delay(500).animate({opacity: 1}, 1000);
+    $('#paid-experience-content').delay(1000).animate({opacity: 1}, 1000);
+    $('#volunteer-experience-header').delay(500).animate({opacity: 1}, 1000);
+    $('#volunteer-experience-content').delay(1000).animate({opacity: 1}, 1000);
+    
+    $('#contact-content').delay(500).animate({opacity: 1}, 1000);
+    
+};
